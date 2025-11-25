122ee5cadadfcb6c40f251eb90869f90212a7dc5
Marco <125201711+MarcoShvy@users.noreply.github.com>
2025-11-24 22:10:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
   return {
     plugins: [react(), tailwindcss()],
     test: {
